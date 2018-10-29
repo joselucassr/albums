d9322881fee462d05ab9e311d15982a8515da45d
Lucas <joselucasrs@gmail.com>
2018-10-29 02:26:49 -0300

    Some big changes, components where added as well as a note directory

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 // Import a library to help create a component
 import React from 'react';
-import ReactNative from 'react-native';
+import { AppRegistry, View } from 'react-native';
+import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
 
 // Create component
-
+const App = () => (
+    <View>
+        <Header headerText={'Albums'} />
+        <AlbumList />
+    </View>
+);
 
 // Render it on the device
+AppRegistry.registerComponent('albums', () => App);
