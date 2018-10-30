5ede679ef414e413821e8333910fadc07c428fa4
Lucas <joselucasrs@gmail.com>
2018-10-29 23:03:42 -0300

    Finished this first project, a lot of things were added

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,16 +1,75 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View, Image, Linking } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
+import Button from './Button';
 
-const AlbumDetail = (props) => {
+const AlbumDetail = ({ album }) => {
+    const { title, artist, thumbnail_image, image, url } = album;
+    const { thumbnailStyle,
+            headerContentStyle,
+            thumbnailConteinerStyle,
+            headerTextStyle,
+            imageStyle
+    } = styles;
+
     return (
         <Card>
+            {/* Thumbnail, title and artist name */}
             <CardSection>
-                <Text>{props.album.title}</Text>
+                <View style={thumbnailConteinerStyle} >
+                    <Image
+                        style={thumbnailStyle}
+                        source={{ uri: thumbnail_image }}
+                    />
+                </View>
+                <View style={headerContentStyle}>
+                    <Text style={headerTextStyle}>{title}</Text>
+                    <Text>{artist}</Text>
+                </View>
+            </CardSection>
+
+            {/* Album image */}
+            <CardSection>
+                <Image
+                    style={imageStyle}
+                    source={{ uri: image }}
+                />
+            </CardSection>
+
+            {/* Button */}
+            <CardSection>
+                <Button onPress={() => Linking.openURL(url)}>
+                    Buy Now
+                </Button>
             </CardSection>
         </Card>
     );
+};
+
+const styles = {
+    headerContentStyle: {
+        flexDirection: 'column',
+        justifyContent: 'space-around'
+    },
+    headerTextStyle: {
+        fontSize: 18
+    },
+    thumbnailStyle: {
+        height: 50,
+        width: 50
+    },
+    thumbnailConteinerStyle: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginRight: 10,
+        marginLeft: 10
+    },
+    imageStyle: {
+        height: 400,
+        flex: 1,
+        width: null
+    }
 };
 
 export default AlbumDetail;
--- src/components/Header.js
+++ src/components/Header.js
@@ -23,7 +23,7 @@ const styles = {
         position: 'relative'
     },
     textStyle: {
-        fontSize: 20
+        fontSize: 24
     }
 };
 
